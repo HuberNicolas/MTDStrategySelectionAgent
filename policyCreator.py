f14a9fcd3f005f812359787202ba9e8fbf4a8b36
Nicolas Huber <nicolas.huber.dev@gmail.com>
2022-08-03 16:37:42 +0200

    refactoring + logging function

---
+++ policyCreator.py
@@ -0,0 +1,79 @@
+import glob
+from multiprocessing import pool
+import os
+import pandas as pd
+import numpy as np
+import shutil
+import csv
+import yaml
+import random
+import subprocess
+import io
+import re
+
+CAT = [
+    'usr',
+    'sys',
+    'idl',
+    'wai',
+    'hiq',
+    'siq',
+    'used',
+    'buff',
+    'cache',
+    'free',
+    'files',
+    'inodes',
+    'read',
+    'writ',
+    'reads',
+    'writs',
+    'recv',
+    'send',
+    'lis',
+    'act',
+    'syn',
+    'tim',
+    'clo',
+    'int',
+    'csw',
+    'run',
+    'blk',
+    'new',
+]
+
+COLS = ['malware', 'metric', 'sign', 'threshold']
+
+
+def createPolicy():
+    with open('config.yaml') as stream:
+        config = yaml.safe_load(stream)
+
+    file = [i for i in glob.glob('./*.csv') if str(config['policy']) in i]
+
+    thresholds = pd.read_csv(file[0], header = None)
+    thresholds.columns = COLS
+    th = thresholds.groupby(['malware'])
+
+    #print(th.get_group('httpbackdoor'))
+
+    policy = pd.DataFrame()
+    for malware in th:
+        if config['random'] == True:
+            # malware is a tuple: (name, df)
+            rows = malware[1].shape[0]
+            nRules = random.choice([config['MIN_TH'], config['MAX_TH']])
+            while(rows < nRules):
+                nRules = random.choice([config['MIN_TH'], config['MAX_TH']])
+
+        else:
+            nRules = config['NUMBER_TH']
+            print('false')
+        #print(malware[1].sample(n = nRules))
+        policy = policy.append(malware[1].sample(n = nRules))
+
+    #print(policy)
+    policy['metric'] = policy['metric'].str.replace('-mean', '')
+    policy = policy.drop_duplicates(subset=['metric']) # todo FIX THIS
+    policy.to_csv('policy.csv', index=False)
+    return policy
--- selectionAgent.py
+++ selectionAgent.py
@@ -10,6 +10,8 @@ import random
 import subprocess
 import io
 import re
+import policyCreator
+import time
 
 CAT = [
     'usr',
@@ -46,100 +48,69 @@ COLS = ['malware', 'metric', 'sign', 'threshold']
 with open('config.yaml') as stream:
     config = yaml.safe_load(stream)
 
-print(config['policy'])
-
-file = [i for i in glob.glob('./*.csv') if str(config['policy']) in i]
-
-thresholds = pd.read_csv(file[0], header = None)
-thresholds.columns = COLS
-th = thresholds.groupby(['malware'])
-
-#print(th.get_group('httpbackdoor'))
-
-policy = pd.DataFrame()
-for malware in th:
-    if config['random'] == True:
-        # malware is a tuple: (name, df)
-        rows = malware[1].shape[0]
-        nRules = random.choice([config['MIN_TH'], config['MAX_TH']])
-        while(rows < nRules):
-            nRules = random.choice([config['MIN_TH'], config['MAX_TH']])
-
-    else:
-        nRules = config['NUMBER_TH']
-        print('false')
-    #print(malware[1].sample(n = nRules))
-    policy = policy.append(malware[1].sample(n = nRules))
-
-#print(policy)
-policy['metric'] = policy['metric'].str.replace('-mean', '')
-policy = policy.drop_duplicates(subset=['metric']) # todo FIX THIS
-policy.to_csv('policy.csv', index=False)
+policy = policyCreator.createPolicy()
 
 # https://stackoverflow.com/questions/1996518/retrieving-the-output-of-subprocess-call
 
 # measure
 from subprocess import PIPE, run
 command = ['dstat', '-t', '--cpu', '--mem', '-d', '--disk-tps', '-n', '--tcp', '-y', '-p', '-N', 'eth0', '1', '1']
-result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-print(result.returncode, result.stdout, result.stderr)
-log = result.stdout
 
-# preprocessing to extract numbers
-start = int(log.find('new'))
-#print(log[start+3+14:-1]) # + 3 (new); + 14 (dd-mm hh:mm:ss|)
-log2=  log[start+3+15:-1]
-print(log2)
+import logging
 
-# extract all numbers
-numbers = re.findall('[0-9.]+[a-zA-Z]|[0-9.]+', log2)
-print(numbers)
+logging.basicConfig(filename='observer.log', filemode='w', format='%(levelname)s - %(message)s', level=logging.INFO)
 
+while True:
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    log = result.stdout
 
-array = []
-for number in numbers:
-    if 'M' in number:
-        print(number)
-        number = float(number[:-1])
-        number = number * 1000 * 1000
-        print(number)
-    elif 'k' in number:
-        print(number)
-        number = float(number[:-1])
-        number = number * 1000
-    else:
-        number = float(number)
+    # preprocessing to extract numbers
+    start = int(log.find('new'))
+    log2=  log[start+3+15:-1]
+    print(result.stdout)
+    
+    
 
-    array.append(number)
-
-# debug
-print(log)
-print(numbers)
-print(array)
-
-#clear
-os.system('cls' if os.name == 'nt' else 'clear')
-
-# iterate over all metrics
-for value, metric in zip(array, CAT):
-
-    #print(value, metric)
-
-    # is there a rule for this metric
-    if metric in set(policy['metric']):
-        #print('found')
-        rule = policy[policy['metric'].str.contains(metric)].values[0]
-        #print(rule)
-        print('{}| Value: {}, Metric: {} {}:'.format(metric, value, rule[2], rule[3]))
-
-        if (rule[2] == '<=') & (float(value) <= float(rule[3])):
-            print('ALERT: we have a {}'.format(rule[0]))
-
-        if (rule[2] == '>=') & (float(value) >= float(rule[3])):
-            print('ALERT: we have a {}'.format(rule[0]))
+    # extract all numbers
+    numbers = re.findall('[0-9.]+[a-zA-Z]|[0-9.]+', log2)
+    moment = re.findall('[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', log)
+    array = []
+    for number in numbers:
+        if 'M' in number:
+            print(number)
+            number = float(number[:-1])
+            number = number * 1000 * 1000
+        elif 'k' in number:
+            number = float(number[:-1])
+            number = number * 1000
         else:
-            print('everything good!')
+            number = float(number)
+
+        array.append(number)
 
 
-    else:
-        print('no rule')
+    # iterate over all metrics
+    for value, metric in zip(array, CAT):
+
+        #print(value, metric)
+
+        # is there a rule for this metric
+        if metric in set(policy['metric']):
+            #print('found')
+            rule = policy[policy['metric'].str.contains(metric)].values[0]
+            #print(rule)
+            print('{}|{}| Value: {}, Metric: {} {}:'.format(moment[0], metric, value, rule[2], rule[3]), end=' ')
+
+            if (rule[2] == '<=') & (float(value) <= float(rule[3])):
+                print('ALERT: we have a {}'.format(rule[0]))
+                logging.critical('{}|{}| Value: {}, Metric: {} {}: we have a {}'.format(moment[0], metric, value, rule[2], rule[3], rule[0]))
+
+            if (rule[2] == '>=') & (float(value) >= float(rule[3])):
+                print('ALERT: we have a {}'.format(rule[0]))
+                logging.critical('{}|{}| Value: {}, Metric: {} {}: we have a {}'.format(moment[0], metric, value, rule[2], rule[3], rule[0]))
+            else:
+                print('everything good!')
+                logging.info('{}|{}|no detection'.format(moment[0], metric))
+        else:
+            print('{}|{}| no rule'.format(moment[0], metric))
+    time.sleep(1)
