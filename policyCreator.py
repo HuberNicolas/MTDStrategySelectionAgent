76fb04be77af6db12a41f05c2a9854e19121d2df
“HuberNicolas” <nicolas.huber2@uzh.ch>
2022-08-09 08:51:29 +0200

    format

--- policyCreator.py
+++ policyCreator.py
@@ -7,7 +7,7 @@ import utils
 
 # load config
 with open('config.yaml') as stream:
-        config = yaml.safe_load(stream)
+    config = yaml.safe_load(stream)
 
 windowSize = config['windowSize']
 randomPolicyCreation = config['randomPolicyCreation']
@@ -17,7 +17,7 @@ maxNumberOfPolicyRules = config['maxNumberOfPolicyRules']
 exactNumberOfPolicyRules = config['exactNumberOfPolicyRules']
 completePolicyCreation = config['completePolicyCreation']
 expertPolicyCreation = config['expertPolicyCreation']
-aggregateFunctions = config['aggregateFunctions'] # avg, min, max
+aggregateFunctions = config['aggregateFunctions']  # avg, min, max
 
 AGGREGATEFUNCTION = aggregateFunctions[0]
 SEED = config['seed']
@@ -27,72 +27,79 @@ POLICYCOLUMNS = utils.POLICYCOLUMNS
 random.seed(SEED)
 print(random.random())
 
+
 def createPolicy():
 
     # load the csv with windowSize
-    filenames = [file for file in glob.glob('./*.csv') if 'policy({}).csv'.format(windowSize) in file]
-    csvPolicy = pd.read_csv(filenames[0], header = None)
-    
+    filenames = [file for file in glob.glob(
+        './*.csv') if 'policy({}).csv'.format(windowSize) in file]
+    csvPolicy = pd.read_csv(filenames[0], header=None)
+
     # postprocess: set header and group by malwaretype
     csvPolicy.columns = POLICYCOLUMNS
-    #print(malwareGroup.get_group('httpbackdoor')) # DEBUG
+    # print(malwareGroup.get_group('httpbackdoor')) # DEBUG
     malwareGroup = csvPolicy.groupby(['malware'])
-   
+
     # policy creation
     policy = pd.DataFrame()
-    
+
     # random policy creation
     # iterate over all malware groups and add some (random or defined) rules (row) for each malware type
     if randomPolicyCreation == True:
         method = 'random'
         if randomNumberOfPolicyRules == True:
-            method += '({}-{})'.format(minNumberOfPolicyRules, maxNumberOfPolicyRules)
+            method += '({}-{})'.format(minNumberOfPolicyRules,
+                                       maxNumberOfPolicyRules)
         else:
             method += '({})'.format(exactNumberOfPolicyRules)
 
-        for malware in malwareGroup:     
+        for malware in malwareGroup:
             # malware is a tuple: (name, df)
-            rows = malware[1].shape[0] # number of rows for that malware type
-            
+            rows = malware[1].shape[0]  # number of rows for that malware type
+
             # random number of rules
             if randomNumberOfPolicyRules == True:
-                
+
                 # define random number between min/max number of policy rules
                 random.seed(SEED)
-                nRules = random.choice([minNumberOfPolicyRules, maxNumberOfPolicyRules])
-                
+                nRules = random.choice(
+                    [minNumberOfPolicyRules, maxNumberOfPolicyRules])
+
             # defined number of rules
             else:
                 nRules = exactNumberOfPolicyRules
-            
+
             # make sure we don't have more rules than rows
-            while(rows < nRules): 
-                    nRules -= 1    
-                
-            
-            #print(malware[1].sample(n = nRules)) # DEBGUG
+            while(rows < nRules):
+                nRules -= 1
+
+            # print(malware[1].sample(n = nRules)) # DEBGUG
             # add defined rules to policy
-            policy = policy.append(malware[1].sample(n = nRules, random_state=SEED)) #todo check what happens if nRules > n when set
+            # todo check what happens if nRules > n when set
+            policy = policy.append(
+                malware[1].sample(n=nRules, random_state=SEED))
             policy = policy.drop_duplicates(subset=['metric'])
-    
+
     # complete policy creation
     # iterate over all malware groups and all rules (row) for each malware type
     elif completePolicyCreation == True:
         method = 'complete'
         policy = csvPolicy
-    
+
     # expert policy creation
     elif expertPolicyCreation == True:
         method = 'expert'
         pass
         # to be done
-        
 
     # postprocessing
-    policy['metric'] = policy['metric'].str.replace('-{}'.format(AGGREGATEFUNCTION), '') # remove aggregate function string for all rows
-    policyName = 'policy({})-{}-{}'.format(windowSize, AGGREGATEFUNCTION, method)
+    # remove aggregate function string for all rows
+    policy['metric'] = policy['metric'].str.replace(
+        '-{}'.format(AGGREGATEFUNCTION), '')
+    policyName = 'policy({})-{}-{}'.format(windowSize,
+                                           AGGREGATEFUNCTION, method)
     policy.to_csv('{}.csv'.format(policyName), index=False)
-    
+
     return policy
 
 
@@ -102,7 +109,7 @@ def malwareDistribution(policy):
     RKMALWALRE = utils.ROOTKIT
     RWMALWARE = utils.RANSOMWARE
     MALWARECATEGORIES = utils.MALWARECATEGORIES
-    
+
     conditions = [
         (policy['malware'].isin(CNCMALWARE)),
         (policy['malware'].isin(RKMALWALRE)),
@@ -110,12 +117,13 @@ def malwareDistribution(policy):
     ]
     # classify each malware by type and add type column
     policy['malwaretype'] = np.select(conditions, MALWARECATEGORIES)
-    
+
     # count different malware types and create a dict
     malwareTypes = policy['malwaretype'].value_counts().index.tolist()
     malwareOccurrences = policy['malwaretype'].value_counts().values.tolist()
-    malwareTypeOcc = {malwareTypes[i]: malwareOccurrences[i] for i in range(len(malwareTypes))}
-    
+    malwareTypeOcc = {malwareTypes[i]: malwareOccurrences[i]
+                      for i in range(len(malwareTypes))}
+
     # count total occurences of all malware types
     totalOccurences = sum(malwareOccurrences)
     '''
@@ -127,5 +135,5 @@ def malwareDistribution(policy):
     totalOccurences: 13
     array([0.53846154, 0.23076923, 0.23076923])
     '''
-    
+
     return [malwareTypeOcc, totalOccurences, np.divide(malwareOccurrences, totalOccurences)]
--- selectionAgent2.py
+++ selectionAgent2.py
@@ -19,8 +19,10 @@ MALWARETYPES = utils.MALWARETYPES
 
 
 formatter = logging.Formatter('%(levelname)s - %(message)s')
+
+
 def setup_logger(name, log_file, level=logging.INFO):
-    handler = logging.FileHandler(log_file)        
+    handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
@@ -28,6 +30,7 @@ def setup_logger(name, log_file, level=logging.INFO):
     logger.addHandler(handler)
 
     return logger
+
 
 # init config
 with open('config.yaml') as stream:
@@ -48,25 +51,33 @@ observer = setup_logger('observer', 'observer.log')
 deployer = setup_logger('deployer', 'deployer.log')
 
 # dstat command
-dstatCommand = ['dstat', '-t', '--cpu', '--mem', '-d', '--disk-tps', '-n', '--tcp', '-y', '-p', '-N', 'eth0', '1', '1'] 
+dstatCommand = ['dstat', '-t', '--cpu', '--mem', '-d',
+                '--disk-tps', '-n', '--tcp', '-y', '-p', '-N', 'eth0', '1', '1']
 
 # MTD policy selection loop
-while True:        
+while True:
     # determine IP
     ipFinder = ['hostname', '-I']
-    ipAddress = run(ipFinder, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    IP = ipAddress.stdout # todo fix mulitple IPs
+    ipAddress = run(ipFinder, stdout=PIPE, stderr=PIPE,
+                    universal_newlines=True)
+    IP = ipAddress.stdout  # todo fix mulitple IPs
     IP = IP.rstrip('\n')
 
     # https://stackoverflow.com/questions/1996518/retrieving-the-output-of-subprocess-call
-    dstatOut = run(dstatCommand, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    dstatOut = run(dstatCommand, stdout=PIPE,
+                   stderr=PIPE, universal_newlines=True)
     observedMetrics = dstatOut.stdout
-    
+
     # preprocessing to metrics
-    start = int(observedMetrics.find('new')) # maybe use 2nd line insead of first
-    observedMetricsProcessed =  observedMetrics[start+3+15:-1]
-    metricsNumbers = re.findall('[0-9.]+[a-zA-Z]|[0-9.]+', observedMetricsProcessed) # extract array of all numbers like 123.32, 1.4B, 34 34K
-    timestamp = re.findall('[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', observedMetrics)[0] # extract timestamp  [dd-mm hh:mm:ss], 01-08 15:13:48
+    # maybe use 2nd line insead of first
+    start = int(observedMetrics.find('new'))
+    observedMetricsProcessed = observedMetrics[start+3+15:-1]
+    # extract array of all numbers like 123.32, 1.4B, 34 34K
+    metricsNumbers = re.findall(
+        '[0-9.]+[a-zA-Z]|[0-9.]+', observedMetricsProcessed)
+    # extract timestamp  [dd-mm hh:mm:ss], 01-08 15:13:48
+    timestamp = re.findall(
+        '[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', observedMetrics)[0]
     # postprocess to array with no postfixes (M, k and B for units)
     systemMetrics = []
     for number in metricsNumbers:
@@ -85,62 +96,68 @@ while True:
 
     # initialize an empty malware indicator table
     malwareIndicators = {
-    'BASHLITE':0,
-    'Ransomware':0,
-    'httpbackdoor':0,
-    'jakoritarleite':0,
-    'The Tick':0,
-    'bdvl':0,
-    'beurk':0
+        'BASHLITE': 0,
+        'Ransomware': 0,
+        'httpbackdoor': 0,
+        'jakoritarleite': 0,
+        'The Tick': 0,
+        'bdvl': 0,
+        'beurk': 0
     }
 
     # iterate over all captures values
     for metricNumber, metric in zip(systemMetrics, METRICSNAME):
-        print(metric, end= ' ')
+        print(metric, end=' ')
         # compare to all existings policy rules
         found = False
         for index, rule in policy.iterrows():
             if metric == rule['metric']:
                 # print(metric, rule['malware'], rule['metric'], metricNumber, rule['sign'], rule['threshold'])
-                
+
                 # falling below critical treshold as indicator
                 if (rule[2] == '<=') & (float(metricNumber) <= float(rule[3])):
-                    print('ALERT: Possible {}'.format(rule[0]), end= ' ')
-                    observer.warning('{}|{}| Value: {}, Metric: {} {:.2f}: Possible {}'.format(timestamp, metric, metricNumber, rule[2], rule[3], rule[0]))
+                    print('ALERT: Possible {}'.format(rule[0]), end=' ')
+                    observer.warning('{}|{}| Value: {}, Metric: {} {:.2f}: Possible {}'.format(
+                        timestamp, metric, metricNumber, rule[2], rule[3], rule[0]))
                     malwareIndicators[rule[0]] += 1
                     found = True
 
                 # exceed critical threshold as indicator
                 if (rule[2] == '>=') & (float(metricNumber) >= float(rule[3])):
-                    print('ALERT: Possible {}'.format(rule[0]), end= ' ')
-                    observer.warning('{}|{}| Value: {}, Metric: {} {:.2f}: Possible {}'.format(timestamp, metric, metricNumber, rule[2], rule[3], rule[0]))
+                    print('ALERT: Possible {}'.format(rule[0]), end=' ')
+                    observer.warning('{}|{}| Value: {}, Metric: {} {:.2f}: Possible {}'.format(
+                        timestamp, metric, metricNumber, rule[2], rule[3], rule[0]))
                     malwareIndicators[rule[0]] += 1
-                    found = True 
-                           
+                    found = True
+
         if not found:
-            print('{}|{}| no detection'.format(timestamp, metric), end= ' ')
+            print('{}|{}| no detection'.format(timestamp, metric), end=' ')
             observer.info('{}|{}|no detection'.format(timestamp, metric))
         print('\n')
 
     # fill malware Indicator dict
-    classifier = dict.fromkeys(classifier, 0) # set all to 0
+    classifier = dict.fromkeys(classifier, 0)  # set all to 0
     for malwareIndicator in malwareIndicators.keys():
-        classifier[MALWARETYPES[malwareIndicator]] += malwareIndicators[malwareIndicator]
+        classifier[MALWARETYPES[malwareIndicator]
+                   ] += malwareIndicators[malwareIndicator]
 
-    # scale number of occurences according to histogram    
+    # scale number of occurences according to histogram
     indicatorResult = ''
     for key in malwareOccurences:
         malwareRatio = malwareOccurences[key] / malwareOccurencesSum
         classifier[key] /= malwareRatio
-        indicatorResult += '{}: {:.2f} '.format(key,classifier[key])
- 
+        indicatorResult += '{}: {:.2f} '.format(key, classifier[key])
+
     # predict
-    predictedType = max(classifier, key=classifier.get)  # todo: insert confidence band (e.g., over 95%)
+    # todo: insert confidence band (e.g., over 95%)
+    predictedType = max(classifier, key=classifier.get)
 
     # create and execute MTDDeployment command
-    triggerMTDCommand = 'python3 /opt/MTDFramework/MTDDeployerClient.py --ip {}--port 1234 --attack {}'.format(IP, predictedType)
-    deployer.critical('{}|Deyploying against {}: {} | {}'.format(timestamp, predictedType, triggerMTDCommand, indicatorResult))
-    #subprocess.call(triggerMTDCommand.split())
-    
-    # wait since sockets seems to have difficulties with to many requests 
+    triggerMTDCommand = 'python3 /opt/MTDFramework/MTDDeployerClient.py --ip {}--port 1234 --attack {}'.format(
+        IP, predictedType)
+    deployer.critical('{}|Deyploying against {}: {} | {}'.format(
+        timestamp, predictedType, triggerMTDCommand, indicatorResult))
+    # subprocess.call(triggerMTDCommand.split())
+
+    # wait since sockets seems to have difficulties with to many requests
     time.sleep(INTERVAL)
