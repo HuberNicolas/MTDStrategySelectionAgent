b21586def27ba8fac87e61e635e929f11f98e4c8
Nicolas Huber <nicolas.huber.dev@gmail.com>
2022-08-14 15:12:56 +0200

    refactoring

--- selectionAgent.py
+++ selectionAgent.py
@@ -6,18 +6,13 @@ import numpy as np
 import yaml
 import subprocess
 import re
-import policyCreator
 import time
 import logging
 from subprocess import PIPE, run
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0', 2345))
-s.listen(10)
+
 # FUNCTIONS
-
-
 def setupLogger(name, log_file, level=logging.INFO):
     handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
@@ -55,87 +50,87 @@ def removeUnits(metricsNumbersArray):
 
     return systemMetrics
 
-
-# CONST
-METRICSNAME = utils.METRICS
-MALWARETYPES = utils.MALWARETYPES
+def indicatorRatio(indicator):
+    # v[0] = # positive hits
+    # v[1] = # negative hits
+    for k, v in indicator.items():
+        # #p == 0 ==> %p:= 0
+        if v[0] == 0:
+            v[2] = 0
+        # p% = #p / (#p + #n)
+        else:
+            v[2] = v[0] / (v[0] + v[1])
 
 # INIT CONFIG
 with open('config.yaml') as stream:
     config = yaml.safe_load(stream)
 
-INTERVAL = config['interval']
-countMalwareIndicators = config['countMalwareIndicators']
-countMalwareTypeIndicators = config['countMalwareTypeIndicators']
-countMalwareIndicatorsRelatively = config['countMalwareIndicatorsRelatively']
 ipFinderCommand = config['ipFinderCommand']
 dstatCommand = config['dstatCommand']
-MODE = config['mode'][0]
+evaluationMethod = config['evaluationMethod']
+DETECTIONTHRESHOLD = config['detectionThreshold']
+HISTORYLEN = config['historyLen']
+RESPONSEPORT = config['responsePort']
+
+# CONST
+METRICSNAME = utils.METRICS
+MALWARETYPES = utils.MALWARETYPES
 
 # INIT LOGGING
 formatter = logging.Formatter('%(levelname)s - %(message)s')
-#logging.basicConfig(filename='observer.log', filemode='w', format='%(levelname)s - %(message)s', level=logging.INFO)
 observer = setupLogger('observer', 'observer.log')
 deployer = setupLogger('deployer', 'deployer.log')
 
-indicator = {
-    'Ransomware': [0, 0, 0],
-    'Rootkit': [0, 0, 0],
-    'CnC': [0, 0, 0],
-}
-
-
-def indicatorRatio(indicator):
-    for k, v in indicator.items():
-        if v[1] == 0:
-            v[2] = 0
-        else:
-            v[2] = v[0] / (v[0] + v[1])
-
 # INIT POLICY
-policy = csvPolicy = pd.read_csv('expert-based-policy.csv', header=None)
+policy = pd.read_csv('expert-based-policy.csv', header=None)
 policy.columns = utils.POLICYCOLUMNS
+
+# INIT SOCKET
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', RESPONSEPORT))
+s.listen(10)
+
 # MTD policy selection loop
 while True:
     startObservationTime = time.time()
     # determine IP
     IP = getIP()
+    
+    # reset indicator
     indicator = {
         'Ransomware': [0, 0, 0],
         'Rootkit': [0, 0, 0],
         'CnC': [0, 0, 0],
     }
+
     # start observation and collect system metrics
-    # https://stackoverflow.com/questions/1996518/retrieving-the-output-of-subprocess-call
     dstatOut = run(dstatCommand, stdout=PIPE,
                    stderr=PIPE, universal_newlines=True)
-    observedMetrics = dstatOut.stdout
-    # process to metrics array with clean numbers
-    # extract 2nd line
-    observedMetricsProcessed = observedMetrics.splitlines()[-1]
-    # extract timestamp  [dd-mm hh:mm:ss], 01-08 15:13:48
+    dstatLines = dstatOut.stdout
+    
+    # extract timestamp from first history entry[dd-mm hh:mm:ss], 01-08 15:13:48
     timestamp = re.findall(
-        '[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', observedMetricsProcessed)[0]
+        '[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', dstatLines.splitlines()[-HISTORYLEN])[0]
     
     history = []
-    # append last 5 entries to history
-    for i in range(-1, -11, -1):
-        history.append(observedMetrics.splitlines()[i])
-
+    # append last N entries to history
+    for i in range(-1, -(HISTORYLEN+1), -1):
+        history.append(dstatLines.splitlines()[i])
     history.reverse() # start with the earliest timestamp
+    
     systemMetricValuesHistory = []
     for h in history:
         # extract timestamp
         timestamp = re.findall(
         '[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', h)[0]
+        
         # extract array of all numbers like 123.32, 1.4B, 34 34K
         metricsNumbers = re.findall(
         '[0-9.]+[a-zA-Z]|[0-9.]+', h[len(timestamp):])
-        # postprocess to array with no postfixes (M, k and B for units)
         
+        # postprocess to array with no postfixes (M, k and B for units)
         systemMetricValues = removeUnits(metricsNumbersArray=metricsNumbers)
         systemMetricValuesHistory.append(systemMetricValues)
-
     
     # calculate average for all metrics for
     systemMetricValues = np.array(systemMetricValuesHistory)
@@ -147,7 +142,7 @@ while True:
         found = False
         for index, rule in policy.iterrows():
             if metricName == rule['metric']:
-                print(metricName, rule['malware'], rule['metric'], metricNumber, rule['sign'], rule['threshold'])
+                # DEBUG # print(metricName, rule['malware'], rule['metric'], metricNumber, rule['sign'], rule['threshold'])
                 found = True
                 # falling below critical treshold as indicator
                 if (rule[2] == '<=') & (float(metricNumber) <= float(rule[3])):
@@ -167,42 +162,49 @@ while True:
                         timestamp, metricName, rule[0]))
 
         if not found:
-            print('{}|{}|No rule'.format(timestamp, metricName))
             observer.info('{}|{}|No rule'.format(timestamp, metricName))
     
+    # predict type
     indicatorRatio(indicator)
-    detectionHiearachy = sorted(
-        indicator.items(), key=lambda i: i[1][0], reverse=True) # [1][0]: sort by pos, [1][2]: sort by %
-    predictedType = detectionHiearachy[0][0]
-    predictedPercentage = detectionHiearachy[0][1][2]
-    resultStr = ''
-    for malwareType in detectionHiearachy:
-        resultStr += '{}:({:.2f}|{:.2f}:{:.2f}), '.format(malwareType[0], malwareType[1][2], malwareType[1][0], malwareType[1][1])
-    detectionHiearachyStr = resultStr[:-2]
+    # sort depending on evaluation method:
+    # [1][0]: sort by # of positive hits e.g. 5 > 2 > 1, [1][2]: sort by % of positive hits 4/6 > 3/6 > 1/3
+    detectionHierarchy = sorted(
+        indicator.items(), key=lambda i: i[1][evaluationMethod], reverse=True) 
+    predictedType = detectionHierarchy[0][0]
+    predictedPercentage = detectionHierarchy[0][1][2]
+    
+    # detection hierarchy: Rootkit:(0.75|3:1), Ransomware:(0.5|1:1), CnC:(0.33|1:2)
+    detectionStr = ''
+    for malwareType in detectionHierarchy:
+        detectionStr += '{}:({:.2f}|{:d}:{:d}), '.format(malwareType[0], malwareType[1][2], malwareType[1][0], malwareType[1][1])
+    detectionHierarchyStr = detectionStr[:-2] # remove '),'
+
+    endObservationTime = time.time()
+    observer.info('{}|Observation took {:.2f}s'.format(
+            timestamp, (endObservationTime - startObservationTime)))
 
     # check threshold
-    endObservationTime = time.time()
-    print('Observation Time: {}s'.format(endObservationTime - startObservationTime))
-
-    if (predictedPercentage >= MODE['detectionTreshold']):
+    if (predictedPercentage >= DETECTIONTHRESHOLD):
         # create and execute MTDDeployment command
         startMTDDeploymentTime = time.time()
         triggerMTDCommand = 'python3 /opt/MTDFramework/MTDDeployerClient.py --ip {}--port 1234 --attack {}'.format(
             IP, predictedType)
 
-        print("deployment")
         deployer.critical('{}|Deyploying against {}: {} |{}'.format(
-            timestamp, predictedType, triggerMTDCommand, detectionHiearachyStr))
-            
+            timestamp, predictedType, triggerMTDCommand, detectionHierarchyStr))
+
+        # start MTD deployment  
         subprocess.call(triggerMTDCommand.split())
-        # wait since sockets seems to have difficulties with to many requests
         
+        # wait for socket response
         client_socket, address = s.accept()
+        mtdTechnique = client_socket.recv(1024).decode('utf-8')
+        # DEBUG # print('Connection ' + address[0])
+        
         endMTDDeploymentTime = time.time()
-        print('Connection ' + address[0])
-        print('Deployment Time: {}s'.format(endMTDDeploymentTime - startMTDDeploymentTime))
+        deployer.info('{}|Deyploying of {} took {:.2f}s'.format(
+            timestamp, mtdTechnique, (endMTDDeploymentTime - startMTDDeploymentTime)))
 
     else:
-        print("no-deployment")
         deployer.info('{}|No deployment against {}: No command was sent |{}'.format(
-            timestamp, predictedType, detectionHiearachyStr))
+            timestamp, predictedType, detectionHierarchyStr))
