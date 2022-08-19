25d2ef7604bbe5e83c2468b022a5947cc363de2a
“HuberNicolas” <nicolas.huber2@uzh.ch>
2022-08-19 14:51:12 +0200

    naming netstat log

--- netstatobserver.sh
+++ netstatobserver.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 observations=300
+now=`date +%F-%H-%M-%S`
+suffix="-netstatlog.txt"
+filename="$path$now$suffix"
 for (( i=1; i<=observations; i++ ))
 do  
   snapshot=`netstat -ent | sed '1d;2d;'`
-  echo $(date +"%Y-%m-%d_%H-%M-%S") >> log.txt
-  echo "$snapshot" >> log.txt
+  echo $(date +"%Y-%m-%d_%H-%M-%S") >> "$filename"
+  echo "$snapshot" >> "$filename"
   sleep 1
 done
